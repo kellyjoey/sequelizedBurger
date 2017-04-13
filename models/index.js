d85a0c0fa3369fdebd31a67359d0f749e02e7d68
Kelly Nicholson <Kelly>
2017-04-12 23:21:12 -0400

    try try again to get a database connection

--- models/index.js
+++ models/index.js
@@ -8,8 +8,8 @@ var env       = process.env.NODE_ENV || 'production';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (config.JawsDB_URL) {
+  var sequelize = new Sequelize(process.env[config.JawsDB_URL]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
